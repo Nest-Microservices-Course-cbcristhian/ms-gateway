9c8c52e6b6eb05ca1958dd86e9020d64ef5e3185
cbcristhian <cristhian.castro@unosquare.com>
2024-06-08 16:02:28 -0400

    crud orders ms

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, HttpStatus, Query, Patch } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { ORDER_SERVICE } from '../config/services';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { StatusDto } from './dto/status.dto';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
@@ -16,12 +18,46 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Query() paginationDto:PaginationDto) {
     return this.ordersClient.send('findAllOrders',{})
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersClient.send('findOneOrder',{id});
+  @Get(':status')
+  findAllByStatus(
+    @Param() statusDto:StatusDto,
+    @Query() paginationDto:PaginationDto
+    ) {
+      try {
+        return this.ordersClient.send('findAllOrders',{
+          ...paginationDto,
+          status:statusDto.status
+        })
+      } catch (error) {
+        throw new RpcException(error)
+      }
+  }
+
+  @Get('id/:id')
+  async findOne(@Param('id',ParseUUIDPipe) id: string) {
+    try {
+      const order= await firstValueFrom(
+        this.ordersClient.send('findOneOrder',{id})
+      )
+      return order
+    } catch (error) {
+      throw new RpcException(error)
+    }
+  }
+
+  @Patch(':id')
+  changeStatus( 
+    @Param('id') id:string,
+    @Body() statusDto:StatusDto,
+  ){
+    try {
+      return this.ordersClient.send('changeOrderStatus',{id,status:statusDto.status})
+    } catch (error) {
+      throw new RpcException(error)
+    }
   }
 }
