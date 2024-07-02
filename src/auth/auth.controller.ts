e2a963f7c405baa819e726c6775583adc76042b7
cbcristhian <cristhian.castro@unosquare.com>
2024-07-01 22:04:09 -0400

    Auth register and login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Inject, Body } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { catchError } from 'rxjs';
 
 
 @Controller('auth')
@@ -14,19 +15,23 @@ export class AuthController{
 
     @Post('register')
     registerUser(@Body()registerUser:RegisterUserDto){
-        this.client.send('auth.register.user',registerUser)
+        return this.client.send('auth.register.user',registerUser).pipe(
+            catchError(error=>{throw new RpcException(error)})
+        )
     }
     
     @Post('login')
     loginUser(@Body() loginDto:LoginDto){
-        this.client.send('auth.login.user',loginDto)
-        
+        return this.client.send('auth.login.user',loginDto).pipe(
+            catchError(error=>{throw new RpcException(error)})
+        )        
     }
     
     @Get('verify')
     verifyToken(){
-        this.client.send('auth.verify.user',{})
-
+        return this.client.send('auth.verify.user',{}).pipe(
+            catchError(error=>{throw new RpcException(error)})
+        )  
     }
 
 }
